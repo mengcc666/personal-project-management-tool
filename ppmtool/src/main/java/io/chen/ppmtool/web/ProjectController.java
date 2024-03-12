cd58c1627177d18ef2b4b01b3938c8de6a5801af
mengcc <1743122693@qq.com>
2024-03-11 20:54:37 -0600

    Add findAll projects api

--- ppmtool/src/main/java/io/chen/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/chen/ppmtool/web/ProjectController.java
@@ -39,4 +39,9 @@ public class ProjectController {
         return new ResponseEntity<Project>(project,HttpStatus.OK);
     }
 
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjests(){
+        return projectService.findAllProjects();
+    }
+
 }
