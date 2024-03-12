28fced1dccd8d127d2c09023f4689369917cff3d
mengcc <1743122693@qq.com>
2024-03-11 20:49:03 -0600

    Return information when id does not exist

--- ppmtool/src/main/java/io/chen/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/chen/ppmtool/services/ProjectService.java
@@ -23,6 +23,10 @@ public class ProjectService {
     }
 
     public Project findProjectByIdentifier(String projectId){
-        return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        Project project=projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project==null){
+            throw new ProjectIdException("Project ID'" + projectId + "' does not exists");
+        }
+        return project;
     }
 }
