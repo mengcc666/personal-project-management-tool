f75548ba0ad19f49d9b4fe9075d8616c7e6c3a1c
mengcc <1743122693@qq.com>
2024-03-12 00:24:01 -0500

    Add basic components on webpage

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
+
+class Dashboard extends Component {
+  render() {
+    return (
+        <div>
+        <h1>Welcome to the dashboard</h1>
+        <ProjectItem/>
+        <ProjectItem/>
+        <ProjectItem/>
+        </div>
+
+    )
+  }
+}
+
+export default Dashboard;
