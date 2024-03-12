245af1b28917c288855f3230e067a369cd89a3e8
mengcc <1743122693@qq.com>
2024-03-11 20:32:26 -0600

    Handle db column=unique exception

--- ppmtool/src/main/java/io/chen/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/chen/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.chen.ppmtool.services;
 
 import io.chen.ppmtool.domain.Project;
+import io.chen.ppmtool.exceptions.ProjectIdException;
 import io.chen.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,14 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
+    public Project saveOrUpdateProject(Project project) {
         // Logic
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier((project.getProjectIdentifier()).toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID'" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+
+        }
     }
 }
